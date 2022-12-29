42853d61304b7c02e10d98054809da7c329cd069
Sangwon <sangwon.choi@bhaptics.com>
2022-12-29 14:26:33 +0900

    Add sample app

---
+++ SampleApp/BhapticsSample/BhapticsSample/BhapticsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BhapticsSampleApp.swift
+//  BhapticsSample
+//
+//  Created by Choi Sangwon on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct BhapticsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleApp/BhapticsSample/BhapticsSample/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  ttttttttttttttttt
+//
+//  Created by Choi Sangwon on 2022/12/29.
+//
+
+import SwiftUI
+import BhapticsExample
+import BhapticsPlugin
+
+let PositionOptions = [BhapticsPosition.Vest, BhapticsPosition.ForearmL
+]
+
+struct ContentView: View {
+    @StateObject private var viewModel = BhapticsViewModelExample.shared
+
+    @State var position = PositionOptions[0]
+    @State var intensityText = "100"
+    @State var intensity = 100
+
+    func toggleScan() {
+        viewModel.toggleScanning()
+    }
+
+    var body: some View {
+        VStack {
+            Button("\(viewModel.kit.isScanning() ? "Scanning" : "Scan")") {
+                toggleScan()
+            }
+            HStack {
+                Picker("Position", selection: $position) {
+                    ForEach(PositionOptions, id: \.self) {
+                        Text($0.name)
+                    }
+                }
+                        .pickerStyle(.automatic)
+
+                TextField("Input", text: $intensityText)
+                        .frame(minWidth: 60)
+                Button("Play") {
+                    let intVal = Int(intensityText)
+                    if (intVal == nil) {
+                        intensity = 100
+                    } else {
+                        intensity = min(100, max(0, intVal!))
+                    }
+                    intensityText = String(intensity)
+
+//                    let motors = [Int](repeating: intensity, count: 20)
+                    let motors = [Int](repeating: intensity, count: 40)
+
+                    viewModel.kit.playMotors(position: position, arr: motors)
+                
+                }
+                        .border(Color.gray)
+
+                Button("Turn Off") {
+                    viewModel.kit.turnOffMotors()
+                }
+                        .border(.gray)
+
+            }
+            
+            DeviceListExample()
+        }
+        .padding()
+        .frame(minWidth: 400)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
